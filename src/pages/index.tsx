83fcbfb69db2a9eed6e9a6f3209c1b618eb58594
viniciustakedi <viniciustakedi7@gmail.com>
2023-04-29 19:08:45 -0300

    Feat: Add description metadata]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,12 @@ export default function Home() {
     <>
       <Head>
         <title>Home • Takedi</title>
+        <meta
+          name="description"
+          content="Olá! Esse é meu portfólio, aqui você pode ver um pouco sobre mim e minhas experiências. 
+          Entre descubra mais sobre mim! e tire as suas dúvidas."
+          key="desc"
+        />
       </Head>
       <main className="min-h-screen bg-gradient-to-r from-dark-black to-dark-blue">
         <Menu />
