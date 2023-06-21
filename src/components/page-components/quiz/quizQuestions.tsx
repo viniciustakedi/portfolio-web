c981b7797eed6ccaacdf074c030af2ef35f53743
viniciustakedi <viniciustakedi7@gmail.com>
2023-06-21 14:05:57 -0300

    Feat: Count down in quiz screen

--- src/components/page-components/quiz/quizQuestions.tsx
+++ src/components/page-components/quiz/quizQuestions.tsx
@@ -1,22 +1,48 @@
-import { currentQuestionContentAtom, isQuizLoadingAtom, quizContentAtom } from "@/contexts/quizzes"
+import { useEffect, useRef, useState } from "react";
+import { currentQuestionContentAtom, isQuizLoadingAtom, quizContentAtom } from "@/contexts/quizzes";
 import { findQuizById, getQuestionById } from "@/services/get";
 import { replyQuizQuestion } from "@/services/patch";
-import { enqueueSnackbar } from "notistack";
-import { useEffect } from "react";
-import { useAtom } from "jotai"
-import Image from "next/image";
 import { shuffleQuizQuestions } from "@/utils";
+import { enqueueSnackbar } from "notistack";
+import { motion } from "framer-motion";
+import { useAtom } from "jotai";
+import Image from "next/image";
 
-export default function QuizQuestions() {  
-  const [currentQuestion, setCurrentQuestion] = useAtom(currentQuestionContentAtom)
-  const [isQuizLoading, setIsQuizLoading] = useAtom(isQuizLoadingAtom)
-  const [quizContent, setQuizContent] = useAtom(quizContentAtom)
+export default function QuizQuestions() {
+  const [currentQuestion, setCurrentQuestion] = useAtom(currentQuestionContentAtom);
+  const [isQuizLoading, setIsQuizLoading] = useAtom(isQuizLoadingAtom);
+  const [quizContent, setQuizContent] = useAtom(quizContentAtom);
+
+  const [countDownValue, setCountDownValue] = useState<number>(3);
+  const [isCountDownActive, setIsCountDownActive] = useState<boolean>(true);
 
   useEffect(() => {
     Promise.resolve().then(async () => {
       await getQuiz();
     });
-  }, [])
+  }, []);
+
+  useEffect(() => { 
+    const interval = setInterval(() => {
+      setCountDownValue((prevCount) => prevCount - 1);
+    }, 1000);
+  
+    if (quizContent.questionsAnswers.length > 0) {
+      setIsCountDownActive(false);
+      clearInterval(interval);
+      return;
+    }
+  
+    if (countDownValue === -1) {
+      setIsCountDownActive(false);
+      clearInterval(interval);
+    }
+  
+    return () => {
+      clearInterval(interval);
+    };
+  }, [countDownValue]);
+  
 
   const getQuiz = async () => {
     const quizId = localStorage.getItem('quizId');
@@ -60,31 +86,62 @@ export default function QuizQuestions() {
 
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen p-4">
-      <div className="lg:w-4/6 md=:w-5/6 w-full">
-        <div className="relative w-full flex mt-8 items-center rounded-md flex-col bg-black">
-          <Image
-            src={currentQuestion.thumbnail}
-            alt="question-thumbnail"
-            className="opacity-20 rounded-md object-cover w-full h-96"
-            width={500}
-            height={250}
-          />
-          <h1 className="absolute lg:top-1/2 md:top-1/2 top-14 lg:text-2xl md:text-2x1 text-lg text-center font-bold" >{currentQuestion.question}</h1>
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-4 mt-4 w-full">
-          {(
-            shuffleQuizQuestions(currentQuestion.options).map((option) => (
-              <button
-                key={option._id}
-                className="bg-blue rounded-md px-4 py-6 text-white w-full font-medium hover:bg-dark-blue transition-all"
-                onClick={() => replyQuestion(option._id)}
-              >
-                {option.value}
-              </button>
-            ))
-          )}
-        </div>
-      </div>
+      {(
+        isCountDownActive && (
+          <motion.h1
+            className="text-6xl font-bold text-white font-italic"
+            key={countDownValue}
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                opacity: 0,
+                y: -50
+              },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.15
+                }
+              }
+            }}
+          >
+            {countDownValue === 0 ? 'GO!' : countDownValue}
+          </motion.h1>
+        )
+      )}
+      {(
+        !isCountDownActive && (
+          <>
+            <div className="lg:w-4/6 md=:w-5/6 w-full">
+              <div className="relative w-full flex mt-8 items-center rounded-md flex-col bg-black">
+                <Image
+                  src={currentQuestion.thumbnail}
+                  alt="question-thumbnail"
+                  className="opacity-20 rounded-md object-cover w-full h-96"
+                  width={500}
+                  height={250}
+                />
+                <h1 className="absolute lg:top-1/2 md:top-1/2 top-14 lg:text-2xl md:text-2x1 text-lg text-center font-bold" >{currentQuestion.question}</h1>
+              </div>
+              <div className="grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-4 mt-4 w-full">
+                {(
+                  shuffleQuizQuestions(currentQuestion.options).map((option) => (
+                    <button
+                      key={option._id}
+                      className="bg-blue rounded-md px-4 py-6 text-white w-full font-medium hover:bg-dark-blue transition-all"
+                      onClick={() => replyQuestion(option._id)}
+                    >
+                      {option.value}
+                    </button>
+                  ))
+                )}
+              </div>
+            </div>
+          </>
+        )
+      )}
     </div>
   )
 }
