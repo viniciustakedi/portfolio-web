77e13e6b52d94501b4bcf378ac86838c20bb9fd0
viniciustakedi <viniciustakedi7@gmail.com>
2023-07-10 16:55:29 -0300

    Chore: Fix hash routes

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -45,27 +45,27 @@ const Menu = () => {
         <div id="navbar-cta" className={`${!isMenuOpen ? 'hidden' : 'visible'} items-center justify-between w-full md:flex md:w-auto md:order-1 transaction-all`}>
           <ul className="flex flex-col font-medium md:p-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 mt-3 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link href="#introduction" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="/#introduction" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Home
               </Link>
             </li>
             <li>
-              <Link href="#habilities" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="/#habilities" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Habilidades
               </Link>
             </li>
             <li>
-              <Link href="#experience" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="/#experience" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Experiência
               </Link>
             </li>
             <li>
-              <Link href="#about-me" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="/#about-me" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Sobre Mim
               </Link>
             </li>
             <li>
-              <Link href="#contact" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+              <Link href="/#contact" scroll={false} onClick={() => setIsMenuOpen(false)} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-soft-blue md:hover:text-dark-blue hover:text-white transition-all md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                 Contato
               </Link>
             </li>
