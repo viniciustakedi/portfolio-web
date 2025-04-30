e1a948f85fba5a93ab1738a3acf8722630c67bec
viniciustakedi <viniciustakedi7@gmail.com>
2025-04-29 21:30:03 -0300

    fix: Skeleton loading to avoid visual bug gliches [NOID]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Work from "./home/components/work";
 import About from "./home/components/about";
 import ContactMe from "./home/components/contact-me";
 import Footer from "@/components/footer";
+import BlurBg from "@/components/blur-bg";
 
 export default function Home() {
   return (
