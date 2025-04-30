5f8d4db7702a91523bb154771d15ac6e0afa0d7f
viniciustakedi <viniciustakedi7@gmail.com>
2025-04-29 21:36:09 -0300

    refactor: Remove unused imports [NOID]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Work from "./home/components/work";
 import About from "./home/components/about";
 import ContactMe from "./home/components/contact-me";
 import Footer from "@/components/footer";
-import BlurBg from "@/components/blur-bg";
 
 export default function Home() {
   return (
